a8849bce21911c2d1920a5fe3f7dad65a6b79940
Jeremy Swallow <jeremy@carverity.com.au>
2025-12-24 13:48:19 +1100

    app.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,48 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
+
+/* Entry points */
 import StartScan from "./pages/StartScan";
+
+/* Online flow */
+import OnlineStart from "./pages/OnlineStart";
 import OnlineScan from "./pages/OnlineScan";
 import OnlineKilometres from "./pages/OnlineKilometres";
+import OnlineOwners from "./pages/OnlineOwners";
 import OnlineAnalyzing from "./pages/OnlineAnalyzing";
 import OnlineReport from "./pages/OnlineReport";
-import InPersonScan from "./pages/InPersonScan";
+
+/* In-person flow */
+import InPersonStart from "./pages/InPersonStart";
+import InPersonPhotos from "./pages/InPersonPhotos";
+import InPersonChecks from "./pages/InPersonChecks";
 import InPersonSummary from "./pages/InPersonSummary";
-import MyScans from "./pages/MyScans";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<StartScan />} />
-          <Route path="/start-scan" element={<StartScan />} />
+    <Routes>
+      <Route element={<Layout />}>
+        {/* Default */}
+        <Route path="/" element={<StartScan />} />
+        <Route path="/start-scan" element={<StartScan />} />
 
-          {/* Online scan */}
-          <Route path="/scan/online" element={<OnlineScan />} />
-          <Route path="/scan/online/kilometres" element={<OnlineKilometres />} />
-          <Route path="/scan/online/analyzing" element={<OnlineAnalyzing />} />
-          <Route path="/scan/online/report/:scanId" element={<OnlineReport />} />
+        {/* ONLINE SCAN FLOW */}
+        <Route path="/scan/online" element={<OnlineStart />} />
+        <Route path="/scan/online/listing" element={<OnlineScan />} />
+        <Route path="/scan/online/kilometres" element={<OnlineKilometres />} />
+        <Route path="/scan/online/owners" element={<OnlineOwners />} />
+        <Route path="/scan/online/analyzing" element={<OnlineAnalyzing />} />
+        <Route path="/scan/online/report" element={<OnlineReport />} />
 
-          {/* In-person scan */}
-          <Route path="/scan/in-person" element={<InPersonScan />} />
-          <Route
-            path="/scan/in-person/summary/:scanId"
-            element={<InPersonSummary />}
-          />
+        {/* IN-PERSON SCAN FLOW */}
+        <Route path="/scan/in-person" element={<InPersonStart />} />
+        <Route path="/scan/in-person/photos" element={<InPersonPhotos />} />
+        <Route path="/scan/in-person/checks" element={<InPersonChecks />} />
+        <Route path="/scan/in-person/summary" element={<InPersonSummary />} />
 
-          {/* Saved scans */}
-          <Route path="/my-scans" element={<MyScans />} />
-
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
   );
 }
