80fcc1de6205c9ded940bceead70d2b1a1fa3e54
Jeremy Swallow <jeremy@carverity.com.au>
2025-12-24 12:45:36 +1100

    app.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -20,35 +20,23 @@ export default function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<StartScan />} />
-
-          {/* Scan entry */}
           <Route path="/start-scan" element={<StartScan />} />
 
           {/* Online scan */}
           <Route path="/scan/online" element={<OnlineScan />} />
-          <Route
-            path="/scan/online/kilometres"
-            element={<OnlineKilometres />}
-          />
-          <Route
-            path="/scan/online/analyzing"
-            element={<OnlineAnalyzing />}
-          />
-          <Route
-            path="/scan/online/report"
-            element={<OnlineReport />}
-          />
+          <Route path="/scan/online/kilometres" element={<OnlineKilometres />} />
+          <Route path="/scan/online/analyzing" element={<OnlineAnalyzing />} />
+          <Route path="/scan/online/report/:scanId" element={<OnlineReport />} />
 
           {/* In-person scan */}
           <Route
-            path="/scan/in-person/summary"
+            path="/scan/in-person/summary/:scanId"
             element={<InPersonSummary />}
           />
 
           {/* Saved scans */}
           <Route path="/my-scans" element={<MyScans />} />
 
-          {/* Fallback */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
