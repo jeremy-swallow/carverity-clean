51c8cf87bf7b4e97387219118423ccd34f8fb709
Jeremy Swallow <jeremy@carverity.com.au>
2025-12-26 17:40:36 +1100

    app.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 
 import Home from "./pages/Home";
@@ -7,14 +7,12 @@ import MyScans from "./pages/MyScans";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/start-scan" element={<StartScan />} />
-          <Route path="/my-scans" element={<MyScans />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/start-scan" element={<StartScan />} />
+        <Route path="/my-scans" element={<MyScans />} />
+      </Routes>
+    </Layout>
   );
 }
