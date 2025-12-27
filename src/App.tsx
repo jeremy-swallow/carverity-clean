a5c40fa13dfb430601818b97b9a47aaeebd4bace
Jeremy Swallow <jeremy@carverity.com.au>
2025-12-28 03:17:39 +1100

    fix: enable deploy-test and credits-history routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,56 +2,59 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 
-/* ---------- Core Pages ---------- */
+/* ---------- MAIN PAGES ---------- */
 import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
 import MyScans from "./pages/MyScans";
-import CreditsHistory from "./pages/CreditsHistory";
+import FAQ from "./pages/FAQ";
 import Account from "./pages/Account";
 
-/* ---------- Online Scan Flow ---------- */
+/* ---------- ONLINE FLOW ---------- */
 import OnlineStart from "./pages/OnlineStart";
 import OnlineDetails from "./pages/OnlineDetails";
 import OnlineAnalyzing from "./pages/OnlineAnalyzing";
 import OnlineResults from "./pages/OnlineResults";
 
-/* ---------- In-Person Scan (Early Steps Only) ---------- */
+/* ---------- IN-PERSON FLOW (active steps) ---------- */
 import InPersonStart from "./pages/InPersonStart";
 import InPersonPhotos from "./pages/InPersonPhotos";
 import InPersonChecks from "./pages/InPersonChecks";
 import InPersonOwners from "./pages/InPersonOwners";
 
-/* ---------- Utility / Temporary Pages ---------- */
+/* ---------- UTIL PAGES ---------- */
+import CreditsHistory from "./pages/CreditsHistory";
 import DeployTest from "./pages/DeployTest";
 
 export default function App() {
   return (
     <Routes>
-
-      {/* Main layout wrapper */}
       <Route element={<Layout />}>
+        {/* Main */}
         <Route path="/" element={<Home />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/my-scans" element={<MyScans />} />
-        <Route path="/credits" element={<CreditsHistory />} />
+        <Route path="/faq" element={<FAQ />} />
         <Route path="/account" element={<Account />} />
 
-        {/* Online scan routes */}
+        {/* Utilities */}
+        <Route path="/credits-history" element={<CreditsHistory />} />
+        <Route path="/deploy-test" element={<DeployTest />} />
+
+        {/* Start Scan Entry */}
+        <Route path="/start-scan" element={<OnlineStart />} />
+
+        {/* Online Scan */}
         <Route path="/scan/online/start" element={<OnlineStart />} />
         <Route path="/scan/online/details" element={<OnlineDetails />} />
         <Route path="/scan/online/analyzing" element={<OnlineAnalyzing />} />
         <Route path="/scan/online/results" element={<OnlineResults />} />
 
-        {/* In-person scan early steps */}
+        {/* In-Person Scan (enabled steps) */}
         <Route path="/scan/in-person/start" element={<InPersonStart />} />
         <Route path="/scan/in-person/photos" element={<InPersonPhotos />} />
         <Route path="/scan/in-person/checks" element={<InPersonChecks />} />
         <Route path="/scan/in-person/owners" element={<InPersonOwners />} />
-
-        {/* Deployment test page */}
-        <Route path="/deploy-test" element={<DeployTest />} />
       </Route>
-
     </Routes>
   );
 }
