e1735f6201ad174bd4d111a325168ff363376870
Jeremy Swallow <jeremy@carverity.com.au>
2026-01-01 12:22:41 +1100

    fix: remove duplicate BrowserRouter to prevent nested router crash

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import StartScan from "./pages/StartScan";
@@ -6,34 +6,30 @@ import OnlineStart from "./pages/OnlineStart";
 import OnlineAnalyzingListing from "./pages/OnlineAnalyzingListing";
 import OnlineVehicleDetails from "./pages/OnlineVehicleDetails";
 import OnlinePhotos from "./pages/OnlinePhotos";
-import OnlineResults from "./pages/OnlineResults";
 import OnlineNextActions from "./pages/OnlineNextActions";
+import OnlineResults from "./pages/OnlineResults";
 import MyScans from "./pages/MyScans";
 import Pricing from "./pages/Pricing";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
 
-        <Route path="/" element={<Home />} />
+      {/* Scan Flow */}
+      <Route path="/start-scan" element={<StartScan />} />
+      <Route path="/scan/online" element={<OnlineStart />} />
+      <Route path="/scan/online/analyzing" element={<OnlineAnalyzingListing />} />
 
-        {/* Scan Flow */}
-        <Route path="/start-scan" element={<StartScan />} />
-        <Route path="/scan/online" element={<OnlineStart />} />
-        <Route path="/scan/online/analyzing" element={<OnlineAnalyzingListing />} />
+      {/* Online Scan Steps */}
+      <Route path="/online/vehicle-details" element={<OnlineVehicleDetails />} />
+      <Route path="/online/photos" element={<OnlinePhotos />} />
+      <Route path="/online/next-actions" element={<OnlineNextActions />} />
+      <Route path="/online/results" element={<OnlineResults />} />
 
-        {/* Online Scan Steps */}
-        <Route path="/online/vehicle-details" element={<OnlineVehicleDetails />} />
-        <Route path="/online/photos" element={<OnlinePhotos />} />
-        <Route path="/online/next-actions" element={<OnlineNextActions />} />
-        <Route path="/online/results" element={<OnlineResults />} />
-
-        {/* Other */}
-        <Route path="/my-scans" element={<MyScans />} />
-        <Route path="/pricing" element={<Pricing />} />
-
-      </Routes>
-    </BrowserRouter>
+      {/* Other */}
+      <Route path="/my-scans" element={<MyScans />} />
+      <Route path="/pricing" element={<Pricing />} />
+    </Routes>
   );
 }
